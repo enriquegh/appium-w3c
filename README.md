5f5c93e058407c5346b7795b58989c683fd1630d
Enrique Gonzalez <enrique@saucelabs.com>
2020-03-24 21:40:18 -0700

    add sauce android web

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 https://github.com/saucelabs/sample-app-mobile/releases/download/2.2.1/Android.SauceLabs.Mobile.Sample.app.2.2.1.apk
 https://github.com/saucelabs/sample-app-mobile/releases/download/2.2.1/iOS.Simulator.SauceLabs.Mobile.Sample.app.2.1.1.zip
 
+NOTE: At the time of writing, Sauce Labs' Android 10 emulator has Chrome 74 which does not support `/actions` commands
 To run:
 ```bash
 npm install
---
+++ config/wdio.sauce.android.web.conf.js
@@ -0,0 +1,22 @@
+const config = require('./wdio.sauce.shared.conf').config;
+const cloneCapabilities = require("../utils")
+
+const NUM_OF_INSTANCES = process.env.WDIO_CAP_MULTIPLIER || 1;
+const baseCapabilityAndroid = {
+    "platformName": "Android",
+    "appium:deviceName": "Android GoogleAPI Emulator",
+    "browserName": "chrome",
+    "platformVersion": "10.0",
+    "appium:deviceOrientation": "portrait",
+    "sauce:options": {
+            "appiumVersion": "1.16.0"
+    }
+  }
+
+config.capabilities = cloneCapabilities(baseCapabilityAndroid, NUM_OF_INSTANCES)
+
+config.specs = [
+    './tests/*-native.ts'
+]
+
+exports.config = config
