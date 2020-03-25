353a537390d542ecf5f9cac03d6974de7d6067e7
Enrique Gonzalez <enrique@saucelabs.com>
2020-03-24 21:36:40 -0700

    add sauce ios native example

---
+++ config/wdio.sauce.ios.native.conf.js
@@ -0,0 +1,23 @@
+const config = require('./wdio.sauce.shared.conf').config;
+const cloneCapabilities = require("../utils")
+
+const NUM_OF_INSTANCES = process.env.WDIO_CAP_MULTIPLIER || 1;
+const baseCapabilityAndroid = {
+    "platformName": "iOS",
+    "appium:deviceName": "iPhone Simulator",
+    "browserName": "",
+    "platformVersion": "13.2",
+    "appium:app": "sauce-storage:swag-labs.app.zip",
+    "appium:deviceOrientation": "portrait",
+    "sauce:options": {
+            "appiumVersion": "1.16.0"
+    }
+  }
+
+config.capabilities = cloneCapabilities(baseCapabilityAndroid, NUM_OF_INSTANCES)
+
+config.specs = [
+    './tests/*-native.ts'
+]
+
+exports.config = config
